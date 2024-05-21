47cc482e805c07e9f5c36c919e2f21a6c3955256
Sheikhman_Kalderon <duah229@outlook.com>
2024-05-21 14:36:55 +0000

    latest: deployed

--- dist/src/routes/formRouter.js
+++ dist/src/routes/formRouter.js
@@ -11,11 +11,11 @@ const authMiddleware_1 = require("../middleWares/authMiddleware");
 const uploadMulter_1 = __importDefault(require("../middleWares/uploadMulter"));
 const router = express_1.default.Router();
 // Route to handle filling the application form with multer middleware
-router.post('/apply', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], uploadMulter_1.default.array('documents'), applicantFormController_1.fillApplicationForm);
+router.post('/apply', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], uploadMulter_1.default.any(), applicantFormController_1.fillApplicationForm);
 router.get('/applications', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], applicantFormController_1.getFormsCreatedByUser);
 // organization
-router.post('/org-apply', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], uploadMulter_1.default.array('documents'), orgApplication_1.createOrganizationForm);
+router.post('/org-apply', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], uploadMulter_1.default.any(), orgApplication_1.createOrganizationForm);
 // JOINT appliaction form
-router.post('/joint-apply', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], uploadMulter_1.default.array('documents'), jointForm_1.jointApplicationForm);
+router.post('/joint-apply', [authMiddleware_1.authMiddleware, authMiddleware_1.applicantMiddleware], uploadMulter_1.default.any(), jointForm_1.jointApplicationForm);
 router.post('/report', applicantFormController_1.createReport);
 exports.default = router;
--- src/routes/formRouter.ts
+++ src/routes/formRouter.ts
@@ -8,14 +8,14 @@ import upload from '../middleWares/uploadMulter'
 const router = express.Router();
 
 // Route to handle filling the application form with multer middleware
-router.post('/apply', [authMiddleware,applicantMiddleware],upload.array('documents'),fillApplicationForm);
+router.post('/apply', [authMiddleware,applicantMiddleware],upload.any(),fillApplicationForm);
 router.get('/applications',[authMiddleware,applicantMiddleware],getFormsCreatedByUser)
 // organization
 
-router.post('/org-apply',[authMiddleware,applicantMiddleware],upload.array('documents'),createOrganizationForm)
+router.post('/org-apply',[authMiddleware,applicantMiddleware],upload.any(),createOrganizationForm)
 
 // JOINT appliaction form
-router.post('/joint-apply',[authMiddleware,applicantMiddleware],upload.array('documents'),jointApplicationForm)
+router.post('/joint-apply',[authMiddleware,applicantMiddleware],upload.any(),jointApplicationForm)
 router.post('/report',createReport)
 
 export default router;
